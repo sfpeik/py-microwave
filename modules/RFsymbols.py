353809552eb4397c6bab9b5112e5b78ae86fdba6
Soeren Peik <speik@hs-bremen.de>
2023-05-25 13:08:38 +0200

    new elements

--- modules/RFsymbols.py
+++ modules/RFsymbols.py
@@ -13,7 +13,7 @@ class Circulator(elm.Element):
             * p3
         '''
         super().__init__(*d, **kwargs)
-        radius = 1.0
+        radius = 0.5
         leadextendfactor = 1.5
         off = radius/sqrt(2)
         off2 = radius*leadextendfactor/sqrt(2)
@@ -21,7 +21,7 @@ class Circulator(elm.Element):
         self.segments.append(Segment([(-off, off), (-off2, off2)]))
         self.segments.append(Segment([(off, off), (off2, off2)]))
         self.segments.append(Segment([(0, -radius), (0, -radius*leadextendfactor)]))
-        self.segments.append(SegmentArc((0,0), 2*radius*0.5, 2*radius*0.5, lw = 2.5, arrow=direction, theta1= 25, theta2=220))
+        self.segments.append(SegmentArc((0,0), 2*radius*0.5, 2*radius*0.5, lw = 4*radius, arrow=direction, theta1= 25, theta2=220))
         self.anchors['p1'] = (-off2,off2)
         self.anchors['p2'] = (off2,off2)
         self.anchors['p3'] = (0, -radius*leadextendfactor)
@@ -30,7 +30,7 @@ class Circulator(elm.Element):
         self.params['lblloc'] = 'lft'
 
 class Isolator(elm.Element):
-    def __init__(self, *d, **kwargs):
+    def __init__(self, size= 1.0, *d, **kwargs):
         '''
 
         Anchors:
@@ -38,16 +38,11 @@ class Isolator(elm.Element):
             * out
         '''
         super().__init__(*d, **kwargs)
-        self.segments.append(Segment([(0, 0 ), (0.5,0)]))
-        self.segments.append(Segment([(0.5, 1 ), (2.5, 1 )]))
-        self.segments.append(Segment([(0.5, -1), (2.5, -1)]))
-        self.segments.append(Segment([(0.5, 1), (0.5, -1)]))
-        self.segments.append(Segment([(2.5, 1), (2.5, -1)]))
-        self.segments.append(Segment([(2.5, 0), (3, 0)]))
-        self.segments.append(SegmentArrow((0.9, 0), (2.3, 0), lw=2.5, headwidth=0.4, headlength=0.4))
+        self.segments.append(Segment([(0, 0 ), (0,0.5), (1, 0.5 ), (1., -0.5 ), (0.0, -0.5), (0, 0)],lw=2))
+        self.segments.append(SegmentArrow((0.2, 0), (0.8, 0), lw=1.5, headwidth=0.2, headlength=0.2))
         self.anchors['in'] = (0, 0)
-        self.anchors['out'] = (3, 0)
-        self.params['drop'] = (3, 0)
+        self.anchors['out'] = (1, 0)
+        self.params['drop'] = (1, 0)
 
 class Port(elm.Element):
     def __init__(self, direction='left', *d, **kwargs):
@@ -61,17 +56,15 @@ class Port(elm.Element):
         size = 0.4
         dir = 1
         if direction =='right': dir = -1
-        self.segments.append(Segment([(0, 0), (-size*dir, -size*0.7)]))
-        self.segments.append(Segment([(0, 0), (-size*dir, size*0.7)]))
-        self.segments.append(Segment([(-size*dir, -size*0.7), (-size*dir, size*0.7)]))
+        self.segments.append(Segment([(0, 0), (-size*dir, -size*0.7),(-size*dir, size*0.7),(0,0)]))
         self.anchors['p'] = (0,0)
         if direction == 'left':
             self.params['lblloc'] = 'lft'
         if direction == 'right':
             self.params['lblloc'] = 'rgt'
 
-class Wilkinson(elm.Element):
-    def __init__(self, showbox = True, *d, **kwargs):
+class Splitter(elm.Element):
+    def __init__(self, showbox = True, size=1 , couplabel="-3dB", *d, **kwargs):
         '''
         Anchors:
             * in
@@ -79,33 +72,27 @@ class Wilkinson(elm.Element):
             *out2
         '''
         super().__init__(*d, **kwargs)
-        size = 1
-        llw = 6
-        self.segments.append(Segment([(0, 0), (size*1,0)],lw=5))
-        self.segments.append(Segment([(size * 1, 0), (size * 2, size)], lw=llw, joinstyle= 'miter'))
-        self.segments.append(Segment([(size * 1, 0), (size * 2, -size)], lw=llw, joinstyle= 'miter'))
-        self.segments.append(Segment([(size * 2, size), (size * 3, size)], lw=llw, joinstyle= 'miter'))
-        self.segments.append(Segment([(size * 2, -size), (size * 3, -size)], lw=llw, joinstyle= 'miter'))
-        self.segments.append(Segment([(size * 2, size), (size * 2, 0.4*size)], lw=3, joinstyle= 'miter'))
-        self.segments.append(Segment([(size * 2, -size), (size * 2, -0.4* size)], lw=3, joinstyle= 'miter'))
-        self.segments.append(Segment([(size * 1.8,  0.4 * size), (size * 2.2, 0.4 *  size)], lw=2))
-        self.segments.append(Segment([(size * 1.8, -0.4 * size), (size * 2.2, 0.4 * -size)], lw=2))
-        self.segments.append(Segment([(size * 1.8,  0.4 * size), (size * 1.8, -0.4 *  size)], lw=2))
-        self.segments.append(Segment([(size * 2.2, 0.4 * size), (size * 2.2, -0.4 * size)], lw=2))
+        llw = 3*size
+        self.segments.append(Segment([(0, 0), (size/2,0)],lw=llw, capstyle="butt",joinstyle= 'miter'))
+        self.segments.append(Segment([(size * 0.5, 0),    (size * 1, size/2)], lw=llw, joinstyle= 'miter'))
+        self.segments.append(Segment([(size * 0.5, 0),    (size * 1, -size/2)], lw=llw, joinstyle= 'miter'))
+        self.segments.append(Segment([(size * 1, size/2), (size * 1.5, size/2)], lw=llw, capstyle="butt", joinstyle= 'miter'))
+        self.segments.append(Segment([(size * 1, -size/2), (size * 1.5, -size/2)], lw=llw, capstyle="butt", joinstyle= 'miter'))
+        ## Coupling Label
+        self.segments.append(SegmentText((size*1,0),couplabel,fontsize = 9*sqrt(size)))
         if showbox:
-            self.segments.append(Segment([(-0.1, 1.5 * size), (size * 3+0.1, 1.5 * size)], lw=1))
-            self.segments.append(Segment([(-0.1, -1.5 * size), (size * 3+0.1, -1.5 * size)], lw=1))
-            self.segments.append(Segment([(-0.1, 1.5 * size), (-0.1 , -1.5 * size)], lw=1))
-            self.segments.append(Segment([(3.1, 1.5 * size), (3.1 , -1.5 * size)], lw=1))
+            self.segments.append(Segment([(0, 0.75 * size), (size * 1.5, 0.75 * size),
+                                          (size * 1.5, -0.75 * size),(0, -0.75 * size),
+                                          (0, 0.75 * size)], lw=2))
         self.anchors['in'] = (0,0)
-        self.anchors['out1'] = (size * 3, size)
-        self.anchors['out2'] = (size * 3, -size)
-        self.params['drop'] = (size * 3, size)
-        self.params['pick'] = (size * 3, size)
+        self.anchors['out1'] = (size * 1.5, size/2)
+        self.anchors['out2'] = (size * 1.5, -size/2)
+        self.params['drop'] = (size * 1.5, size/2)
+        self.params['pick'] = (size * 1.5, size/2)
 
 
 class Coupler(elm.Element):
-    def __init__(self, showbox = True, *d, **kwargs):
+    def __init__(self, showbox = True, size= 1, *d, **kwargs):
         '''
         Anchors:
             * p1
@@ -114,19 +101,16 @@ class Coupler(elm.Element):
             * p4
         '''
         super().__init__(*d, **kwargs)
-        size = 0.5
+        size = 0.5 * size
         llw = 6
-        self.segments.append(Segment([(0, 0), (size * 3, 0)], lw=6*size))
-        self.segments.append(Segment([(0, -2*size), (size * 3, -2*size)], lw=6*size))
+        self.segments.append(Segment([(0, 0), (size * 3, 0)], capstyle="butt", lw=6*size))
+        self.segments.append(Segment([(0, -2*size), (size * 3, -2*size)], capstyle="butt", lw=6*size))
         self.segments.append(SegmentArrow((size * 0.3, -0.3 * size), (size * 2.6, -1.7 * size), lw=2*size)) 
         self.segments.append(SegmentArrow((size * 0.3, -1.7 * size), (size * 2.6, -0.3 * size), lw=2*size))
         self.segments.append(SegmentArrow((size * 2.6, -1.7 * size), (size * 0.3, -0.3 * size), lw=2*size)) # Reverse directions
         self.segments.append(SegmentArrow((size * 2.6, -0.3 * size), (size * 0.3, -1.7 * size), lw=2*size))
         if showbox:
-            self.segments.append(Segment([(size * 0, 0.5 * size), (size * 3, 0.5 * size)], lw=1))
-            self.segments.append(Segment([(size * 0, -2.5 * size),(size * 3, -2.5 * size)], lw=1))
-            self.segments.append(Segment([(size * 0, 0.5 * size), (size * 0 , -2.5 * size)], lw=1*size))
-            self.segments.append(Segment([(size * 3, 0.5 * size),  (size * 3 , -2.5 * size)], lw=1*size))
+            self.segments.append(Segment([(0, 0.5 * size), (size * 3, 0.5 * size),(size * 3, -2.5 * size),(size * 0 , -2.5 * size),(0, 0.5 * size)], lw=2))
 
         self.anchors['p1'] = (0, 0)
         self.anchors['p2'] = (size*3, 0)
@@ -138,22 +122,32 @@ class Coupler(elm.Element):
  
 if __name__ == "__main__":
     import schemdraw as schem
+    import schemdraw.dsp as dsp
     d = schem.Drawing()
-    d += Port().label("Port1")
+    d += Port().label("Port1").fill('skyblue')
     d += elm.Line(l=1)
-    d += ( cir1 := Circulator('cw').flip().anchor('p1') )
-    d += Port(direction='right').up().at(cir1.p3).label("Port2")
+    d += ( cir1 := Circulator('cw').flip().anchor('p1').fill('peachpuff') )
+    d += Port(direction='right').up().at(cir1.p3).label("Port2").fill('skyblue')
     d += elm.Line(l=1).at(cir1.p2)
-    d += elm.Coax(l=1).label("$\lambda/4$")
-    d += (wilk := Wilkinson().label("Wilkinson\nDivider\n",fontsize=10) )
-    d += Isolator().reverse()
-    d += (cou := Coupler().label("Forward\nCoupler\n10 dB\nno Phase Shift", fontsize=10) )
+    d += elm.Coax(l=1,radius= 0.2).label("$\lambda/4$")
+    d += (wilk := Splitter(size=1.3).label("Wilkinson\nDivider\n",fontsize=10).fill('Khaki'))
+    d += elm.Line(l=1)
+    d += Isolator().fill('lightgray')
+    d += elm.Line(l=1)
+    d += (cou := Coupler(size=1.3).label("Forward\nCoupler\n10 dB\nno Phase Shift", fontsize=10).fill('Khaki'))
     d.push()
-    d += elm.Line(l=3).at(wilk.out2)
+    d += elm.Line(l=1).at(wilk.out2)
+    d += dsp.Filter().fill('lavenderblush')
+    d += elm.Line(l=1).right()
     d.pop()
-    d += elm.Coax(l=1).label("$\lambda/4$")
-    d += Isolator().label("Isolator", fontsize=10)
-    d += Port().left().label("Port3", loc='right')
-    d += Isolator().at(cou.p3).reverse()
-    d += Port().left().label("Port4", loc='right')
+    d += elm.Coax(l=1,radius=0.2).label("$\lambda/4$")
+    d += dsp.Amp().label("LNA", fontsize=10).fill('linen')
+    d += elm.Line(l=0.5)
+    d += Port().left().label("Port3", loc='right').fill('skyblue')
+    d += elm.Line(l=1).at(cou.p3)
+    d += Isolator().reverse().fill('lightgray')
+    d += elm.Line(l=2.5)
+    d += Port().left().label("Port4", loc='right').fill('skyblue')
     d.save('example.png',dpi=600)
+
+
